74c72ec1c690de5f2857dd79902b7dbbadc40fb0
Ashwani Kushwaha <ashwanik346981@gmail.com>
2025-09-11 10:48:05 +0530

    add careerAssessment Page

---
+++ src/app/careerAssessment/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CareerAssessment = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center text-3xl font-bold'>
+        CareerAssessment
+    </div>
+  )
+}
+
+export default CareerAssessment
